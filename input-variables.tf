e4cb4895db7d88bd00c5887074ab7d3ee71cbbb7
sankusinha <47401612+sankusinha@users.noreply.github.com>
2023-01-30 12:56:37 +0530

    Update and rename generic-input-variables.tf to input-variables.tf

--- input-variables.tf
+++ input-variables.tf
@@ -40,3 +40,25 @@ variable "vnet_address_space" {
   type        = list(string)
   default     = ["10.0.0.0/16"]
 }
+/* State file related variables
+variable "state_rg" {
+  description = "State Resource Group Name"
+  type        = string
+  //default     = "terraform-state-rg"
+}
+variable "state_sa" {
+  description = "State Storage Account Name"
+  type        = string
+  //default     = "tfstatesanku"
+}
+variable "state_container" {
+  description = "State Container Name"
+  type        = string
+  //default     = "tfstatefiles"
+}
+variable "state_key" {
+  description = "State Key Name"
+  type        = string
+  //default     = "dev-terraform.tfstate"
+}
+*/
