6dac26fa58c0f9b8191c9819a8aa8682a23f24ca
sankusinha <47401612+sankusinha@users.noreply.github.com>
2023-02-20 12:57:55 +0530

    Update input-variables.tf

--- input-variables.tf
+++ input-variables.tf
@@ -1,23 +1,16 @@
 # Environment Variable
-variable "environment" {
-  description = "Environment Variable used as a prefix"
-  type        = string
-  default     = "dev"
-}
+#variable "environment" {
+#  description = "Environment Variable used as a prefix"
+#  type        = string
+#  default     = "dev"
+#}
 
 # Azure Resource Group Name 
-variable "resource_group_name" {
-  description = "Resource Group Name"
-  type        = string
-  default     = "rg-scalr"
-}
-
-# Azure Resources Location
-variable "resource_group_location" {
-  description = "Region in which Azure Resources to be created"
-  type        = string
-  default     = "eastus"
-}
+#variable "resource_group_name" {
+#  description = "Resource Group Name"
+#  type        = string
+#  default     = "rg-scalr"
+#}
 
 variable "tags" {
   type = map(string)
